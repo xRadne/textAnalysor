c388963f86dcf517e1eb8baa3d0fc2cc6260ce21
xRadne <Alex.radne@gmail.com>
2018-03-27 19:57:49 +0200

    input case handling

--- textAnalysor.rb
+++ textAnalysor.rb
@@ -1,12 +1,35 @@
-def Analyse(path)
+def Analyse(text)
     freqs = {}
     freqs.default = 0
     
-    File.open(path).each do |line|
-        line.each_char {|char| freqs[char] += 1}
-    end
-    puts "Frequency of every character in '#{path}': "
+    text.each_char {|char| freqs[char] += 1}
+    puts "Frequency of every character in text: "
     ("a".."z").each {|char| puts "#{char} : #{freqs[char]}"}
 end
 
-analyse("example.txt")
+def GetTextFrom(path)
+    text = ""
+    File.open(path).each do |line|
+        text += line
+    end 
+    return text
+end
+
+puts "Text analyser!"
+puts "--------------"
+puts "Input som text to analyse"
+puts "You can also input a path starting by '/'"
+print ": "
+input = gets.chomp
+
+case input[0]
+when nil
+    puts "No path or text was given. Runs example.txt instead"
+    Analyse("example.txt")
+when "/"
+    input.slice!(0)
+    puts "Analysing path: '#{input}'"
+    Analyse(GetTextFrom(input))
+else
+    Analyse(input)
+end
