8e339ad796079051d08815ea36683d48917faacf
xRadne <Alex.radne@gmail.com>
2018-03-27 19:20:41 +0200

    initial def Analyse

--- textAnalysor.rb
+++ textAnalysor.rb
@@ -1,8 +1,12 @@
-print "Write a text to analyse: "
-text = gets.chomp
-text.downcase!
-freqs = {}
-freqs.default = 0
-text.each_char {|char| freqs[char] += 1}
-puts "Frequency of every character: "
-("a".."z").each {|char| puts "#{char} : #{freqs[char]}"}
+def Analyse(path)
+    freqs = {}
+    freqs.default = 0
+    
+    File.open(path).each do |line|
+        line.each_char {|char| freqs[char] += 1}
+    end
+    puts "Frequency of every character in '#{path}': "
+    ("a".."z").each {|char| puts "#{char} : #{freqs[char]}"}
+end
+
+analyse("example.txt")
