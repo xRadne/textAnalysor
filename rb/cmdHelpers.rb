b416cd621b78566ad8856cddbc52a09e361fb5a8
xRadne <Alex.radne@gmail.com>
2018-04-01 16:09:27 +0200

    call with arguments

--- rb/cmdHelpers.rb
+++ rb/cmdHelpers.rb
@@ -1,10 +1,11 @@
-def askFor(q, yesMethod, noMethod)
+def askFor(q, yesMethod, noMethod, args)
     print q + " [Y/N]: "
     input = gets.chomp
+    input = 'Y' if input == ''
     case input[0]
     when 'Y', 'y'
-        yesMethod.call()
+        yesMethod.call(args)
     when 'N', 'n'
-        noMethod.call()
+        noMethod.call(args)
     end
 end
